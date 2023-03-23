24f3b17ee869ef99b95017a35bcf7540d1e7c534
julietacolque <yuliicolque@gmail.com>
2023-03-23 00:01:37 -0300

    ejercicios de punteros

--- tp1_1.c
+++ tp1_1.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+
 int main(){
+    int numero=12;
+    int *punt;
+    punt = &numero;
     printf("Hola mundo");
+
+    printf("\nEl contenido del puntero es: %d", punt); //contenido del punt direccion de memoria de num
+    printf("\nLa direccion de memoria almacenada por el puntero: %d",punt);
+    printf("\nLa direccion de memoria de la variable: %d",&numero);
+    printf("\n Direccion de memoria del puntero: %p",&punt);
+    printf("\nEl tamanio de la memoria usado es: %d", sizeof(punt));
     return 0;
 }
